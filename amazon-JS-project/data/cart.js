3200d75343eac4c1e0bfaf392f1464006df1e624
Ravindra Babu Devabhaktuni <rbabu.devabhaktuni79@Gmail.com>
2024-12-11 20:37:45 -0600

    Delete products from cart when click delete button

--- amazon-JS-project/data/cart.js
+++ amazon-JS-project/data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
 
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
@@ -32,5 +32,21 @@ export function addToCart([productId]){
     quantity: 1
    });
   }
+
+}
+//The below function takes the product id and remove it
+/* How?
+1. Create a new array
+2. Loop through the cart
+3. Add each product to the new array , except for this productId
+*/
+export function removeFromCart(productId){
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if(cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+cart = newCart;
 
 }
--- amazon-JS-project/scripts/checkout.js
+++ amazon-JS-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -41,7 +41,7 @@ cart.forEach((cartItem) => {
         <span class="update-quantity-link link-primary">
           Update
         </span>
-        <span class="delete-quantity-link link-primary">
+        <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchingProduct.id}">
           Delete
         </span>
       </div>
@@ -99,3 +99,18 @@ cart.forEach((cartItem) => {
 });
 //console.log(cartSummaryHTML);
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+
+      const productId = link.dataset.productId;
+
+      // console.log(productId); output the product Id if we click the delete button
+      // ex: e43638ce-6aa0-4b85-b27f-e1d07eb678c6
+
+      removeFromCart(productId);
+      //console.log(cart);
+
+    });
+  });

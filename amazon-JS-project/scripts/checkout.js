8af003b5d29500e903f8ec07407a11eaff481558
Ravindra Babu Devabhaktuni <rbabu.devabhaktuni79@Gmail.com>
2024-12-15 15:22:02 -0600

    delivery date

--- amazon-JS-project/scripts/checkout.js
+++ amazon-JS-project/scripts/checkout.js
@@ -2,6 +2,11 @@ import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(deliveryDate.format('dddd, MMMM D'));
+
+
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {

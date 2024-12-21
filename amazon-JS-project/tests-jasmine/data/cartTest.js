8e537243a985a2c936108322aec472f74dde9992
Ravindra Babu Devabhaktuni <rbabu.devabhaktuni79@Gmail.com>
2024-12-21 13:50:34 -0600

    created autoomated tests for addToCart - mock localStorage

--- amazon-JS-project/tests-jasmine/data/cartTest.js
+++ amazon-JS-project/tests-jasmine/data/cartTest.js
@@ -3,19 +3,32 @@ import {addToCart, cart, loadFromStorage} from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
+    spyOn(localStorage, 'setItem')
 
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);  
+    });
+    loadFromStorage();
+
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(2);
 
   });
 
   it('adds a new product to the cart', () => {
-    spyOn(localStorage, 'setItem').and.callFake(() => {
-      return parseFloat.JSON
-    });
+    spyOn(localStorage, 'setItem');
 
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);  
     });
-    console.log(localStorage.getItem('cart')); // []
+    //console.log(localStorage.getItem('cart')); // []
     loadFromStorage();
     //Here, we mock localStorage.getItem  first into an emprty array and then we reload the cart. so now cart will be empty
 
@@ -24,6 +37,9 @@ describe('test suite: addToCart', () => {
     //adding one product to the cart
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
   });
 
 });

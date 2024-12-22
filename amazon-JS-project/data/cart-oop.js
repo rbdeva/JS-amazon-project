f05ca0bccbb2fc68c4b04b6c31e1aa306e8f407b
Ravindra Babu Devabhaktuni <rbabu.devabhaktuni79@Gmail.com>
2024-12-22 02:21:52 -0600

    18:03

---
+++ amazon-JS-project/data/cart-oop.js
@@ -0,0 +1,112 @@
+function Cart(){
+  const cart = {
+    //property: value
+    //cart: undefined;
+   cartItems: undefined,
+    
+    //loadFromStorage: function(){ - shotcut for the beolowline code
+    loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem('cart-oop'));
+    
+    if (!this.cartItems) {
+      this.cartItems = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+      }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+      }];
+    }
+    },
+
+      //function saveToStorage() {
+      //saveToStorage: function(){ - shotcut for the beolowline code
+      saveToStorage(){
+      localStorage.setItem('cart-oop', JSON.stringify(this.cartItems));
+    },
+
+    //export function addToCart(productId){
+    //addToCart(productId): function{ - shotcut for the beolowline code
+    addToCart(productId){
+      let matchingItem;
+    
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+    
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        this.cartItems.push({
+          productId: productId,
+          quantity: 1,
+          deliveryOptionId: '1'
+        });
+      }
+    
+      this.saveToStorage();
+    },
+    
+    //export function removeFromCart(productId) {
+    //removeFromCart(productId): function(){
+    removeFromCart(productId) {
+      const newCart = [];
+    
+      this.cartItems.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+          newCart.push(cartItem);
+        }
+      });
+    
+      this.cartItems = newCart;
+    
+      this.saveToStorage();
+    },
+
+    //export function updateDeliveryOption(productId, deliveryOptionId) {
+    //
+    updateDeliveryOption(productId, deliveryOptionId){
+      let matchingItem;
+    
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+    
+      matchingItem.deliveryOptionId = deliveryOptionId;
+    
+      this.saveToStorage();
+    }
+
+};
+  return cart;
+}
+
+const cart = Cart();
+const businessCart = Cart();
+
+
+cart.loadFromStorage();
+
+
+businessCart.loadFromStorage();
+
+console.log(cart);
+console.log(businessCart);
+
+
+
+
+
+
+
+
+
+
+
+
